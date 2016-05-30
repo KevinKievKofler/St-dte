a9098a43fac9c6fb8692c52477925fac3c705a10
KevinKievKofler <koflerkevin@yahoo.de>
2016-05-30 16:00:39 +0200

    Update this rubish

--- Städte/MainWindow.xaml.cs
+++ Städte/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.OleDb;
 
 namespace Städte
 {
@@ -20,9 +22,44 @@ namespace Städte
     /// </summary>
     public partial class MainWindow : Window
     {
+        string conn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=db_EUStaaten.mdb";
+        string cmd = "SELECT * FROM tbl_EUStaaten";
+        DataSet ds = new DataSet();
+        DataSet dx = new DataSet();
+        string landkurz;
         public MainWindow()
         {
             InitializeComponent();
+            try
+            {
+                OleDbDataAdapter da = new OleDbDataAdapter(cmd, conn);
+                da.Fill(ds, "tbl_Land");
+            }
+            catch(Exception f)
+            {
+                MessageBox.Show(f.Message);
+                this.Close();
+            }
+            DataTable tbl1 = ds.Tables["tbl_Land"];
+            cmb.ItemsSource = tbl1.DefaultView;
+        }
+
+        private void cmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            landkurz = (string)cmb.SelectedValue;
+            cmd = string.Format("SELECT * FROM tbl_Staedte WHERE Land = \"{0}\"", landkurz);
+            try
+            {
+                OleDbDataAdapter datacon = new OleDbDataAdapter(cmd, conn);
+                datacon.Fill(dx, "tbl_stadt");
+            }
+            catch(Exception f)
+            {
+                MessageBox.Show(f.Message);
+                this.Close();
+            }
+            DataTable tbl1 = dx.Tables["tbl_stadt"];
+            daten.ItemsSource = tbl1.DefaultView;
         }
     }
 }
